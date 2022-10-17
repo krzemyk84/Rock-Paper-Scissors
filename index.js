ed18aa7a85e4c1d540686f535b32c03dae99da31
krzemyk <113865837+krzemyk84@users.noreply.github.com>
2022-10-17 15:18:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+  function inputUsername() {
+    const username = prompt ("Please enter your username:");
+    user = username;
+    // alert(username);
+    //transcription(user) this is how to pass an argument to another function
+  }
+  function getRandomArbitrary (min, max) {
+    var min = 1;
+    var max = 13; 
+      return x = Math.random() * (max - min) + min;
+    alert (x);
+  }
+
+  function transcription() { // this function changes numbers to card names
+    var card = "";
+    
+   
+    switch (stringNumber) {
+      case "1":
+        card = "Ace";
+        break;
+      case "11":
+        card = "Jack";
+        break;
+      case "12":
+        card = "Queen";
+        break;
+      case "13":
+        card = "King";
+        break;
+      default:
+        card = stringNumber;
+    }
+    cardTranscription = card;
+    alert(stringNumber);
+  }
+
+
+
+  inputUsername();
+  getRandomArbitrary();
+   randomNumber = parseInt(x);
+   stringNumber = randomNumber.toString();
+  var user;
+  var cardTranscription;
+  transcription();
+  alert (user + " you drew a " + cardTranscription);
